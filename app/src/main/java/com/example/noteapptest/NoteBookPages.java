21be6fe719c1fe221d355d8419d5f36ab988cb72
Seamus-Grady <?`4$d4^^XAJgD"sW>
2020-03-24 16:34:52 -0600

    Modified AddNoteBookPages layout to now have a title textbox before the listView to Name the notebook. Hooked up all the activities and modified the addNoteBookPage Menu

--- app/src/main/java/com/example/noteapptest/NoteBookPages.java
+++ app/src/main/java/com/example/noteapptest/NoteBookPages.java
@@ -1,14 +1,95 @@
 package com.example.noteapptest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class NoteBookPages extends AppCompatActivity {
+
+    private ListView listView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_book_pages);
+        listView = findViewById(R.id.noteBookPages);
+        editText = findViewById(R.id.noteBookName);
+
+        editText.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_UP && !v.hasFocus())
+                {
+                    v.performClick();
+                }
+                return false;
+            }
+        });
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if(editText.getText().toString().equals("Title"))
+                {
+                    editText.setText("");
+                    editText.setSelection(editText.getText().length());
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_a_page_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.Save:
+                this.setTitle(editText.getText().toString());
+                findViewById(R.id.noteBookPageLayoutActivity).requestFocus();
+                hideAKeyboard(this);
+                return true;
+            case R.id.Add_Page:
+                Intent intent = new Intent(getApplicationContext(), PageActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private static void hideAKeyboard(Activity activity)
+    {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
